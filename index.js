237f6453b77a212522201d45cddc8a385381a7db
G4rutti <davigarutti5@gmail.com>
2023-01-08 23:23:28 -0300

    adicionando mais elementos ao data

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ const main = async () => {
     const $ = cheerio.load(content)
     let artilheiros = []
 
-    $('table.items tbody tr').each((i, e) => {
+    $('#yw1 > table.items > tbody > tr').each((i, e) => {
         const AllElements = $(e).find('td')
         const posicaoRanking = $(AllElements[0]).text()
+        const imagemArtilheiro = $(AllElements[2]).find('img').attr('src')
         const nomeArtilheiro = $(AllElements[1]).find('table > tbody > tr > td.hauptlink > a').text()
         const nacionalidade = $(AllElements[5]).find('img').attr('title')
         const idade = $(AllElements[6]).text()
         const clube = $(AllElements[7]).find('a').attr('title')
+        const imagemClube = $(AllElements[7]).find('a > img').attr('src')
         const jogos = $(AllElements[8]).find('a').text()
         const gols = $(AllElements[9]).find('a').text()
         
 
         if(nomeArtilheiro != ""){
-            const data = {posicaoRanking, nomeArtilheiro, nacionalidade, idade, clube, jogos, gols}
+            const data = {posicaoRanking, imagemArtilheiro, nomeArtilheiro, nacionalidade, idade, clube, imagemClube, jogos, gols}
             artilheiros.push(data)
             console.log(data)
         }
